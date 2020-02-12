1354e61bf29c43279c2a6dcfbb18824feb8ac8de
maks1m_p <maksym.pohribniak@sixt.com>
2020-02-13 01:55:20 +0200

    share md

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+(function(){
+    const btn = document.getElementById('share-btn');
+
+    btn.addEventListener('click', () => {
+        const loc = window.location.href;
+
+        if (navigator.share) {
+            navigator.share({
+                title: '#DrinksOnD smarturl.it/DrinksonMe',
+                text: '#DrinksOnD smarturl.it/DrinksonMe',
+                url: `${loc}/assets/DMunoz_TextGif3.mp4`,
+            });
+        }
+    });
+})()
