f5bcc8ac47da14126b02166845757b11fa6d7daa
maks1m_p <maksym.pohribniak@sixt.com>
2020-02-13 02:26:01 +0200

    last minute changes

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,12 @@
 
     btn.addEventListener('click', () => {
         const loc = window.location.href;
-
+        
         if (navigator.share) {
             navigator.share({
                 title: '#DrinksOnD smarturl.it/DrinksonMe',
                 text: '#DrinksOnD smarturl.it/DrinksonMe',
-                url: `${loc}/assets/DMunoz_TextGif3.mp4`,
+                url: `${loc}/assets/DMunoz_share_love.gif`,
             });
         }
     });
