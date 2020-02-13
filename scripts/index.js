f4cb79f05ff8add3d773c850a0143b78c8771b2d
maks1m_p <maksym.pohribniak@sixt.com>
2020-02-13 10:48:23 +0200

    add get lova button

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,17 @@
 (function(){
-    const btn = document.getElementById('share-btn');
-
-    btn.addEventListener('click', () => {
-        const loc = window.location.href;
-        
-        if (navigator.share) {
-            navigator.share({
-                title: '#DrinksOnD smarturl.it/DrinksonMe',
-                text: '#DrinksOnD smarturl.it/DrinksonMe',
-                url: `${loc}/assets/DMunoz_share_love.gif`,
-            });
+    const loc = window.location.href;
+    const shareContent = fileName => event => {
+        if (event) {
+            event.preventDefault();
         }
-    });
+        
+        navigator.share({
+            title: '#DrinksOnD smarturl.it/DrinksonMe',
+            text: '#DrinksOnD smarturl.it/DrinksonMe',
+            url: `${loc}/assets/${fileName}`,
+        });
+    };
+
+    document.getElementById('share-btn')
+        .addEventListener('click', shareContent('DMunoz_share_love.gif'));
 })()
